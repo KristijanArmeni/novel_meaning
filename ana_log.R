81202fe8966f85cc6ab4abe3ca343df1a1970a87
KristijanArmeni <kristijan.armeni@gmail.com>
2016-02-01 18:16:09 +0100

    initial commit

---
+++ ana_log.R
@@ -0,0 +1,45 @@
+#NOVEL_MEANING
+
+#this is the script for analysing individual emotion/attention/motivation ratings
+
+library(ggplot2)
+library(reshape)
+curr_dir = getwd()
+
+inp_dir = 'D:/Kristijan/raziskovalno/novel_meaning/data/logs'
+out_dir = 'D:/Kristijan/raziskovalno/novel_meaning/data/logs'
+
+# set proper dir for reading in the files
+if (curr_dir != inp_dir){
+  setwd(inp_dir)
+}
+
+###----------------------------------------------------------------------###
+
+# read in the data
+my_data <- read.table("Ratings.txt", header = TRUE)
+
+## RESHAPE THE DATA
+my_data$mood <- factor(my_data$mood, levels = c(1,2), labels = c("happy", "sad"))
+
+#ratings
+ratings <- data.frame(ID = my_data$sID,
+                  group = my_data$mood,
+                  BL = my_data$E1,
+                  mip1 = my_data$E2,
+                  mip2 = my_data$E3,
+                  mip3 = my_data$E4,
+                  att1 = my_data$A1,
+                  att2 = my_data$A2,
+                  mot1 = my_data$M1,
+                  mot2 = my_data$M2)
+
+#get summary statistics
+sum_stat_BL <- aggregate(ratings$BL, by = list(ratings$group), FUN = summary)
+sum_stat_mip1 <- aggregate(ratings$mip1, by = list(ratings$group), FUN = summary)
+sum_stat_mip2 <- aggregate(ratings$mip2, by = list(ratings$group), FUN = summary)
+sum_stat_mip3 <- aggregate(ratings$mip3, by = list(ratings$group), FUN = summary)
+
+#summary plots
+summary_plot <- ggplot(data = ratings, aes(x = ratings$mip1,))
+
