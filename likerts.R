4cf75e922c3b54f103baed6c98445ee07e04aa2d
KristijanArmeni <kristijan.armeni@gmail.com>
2016-02-17 20:50:53 +0100

    changed ggplot, added some reshaping stuff

--- likerts.R
+++ likerts.R
@@ -12,6 +12,7 @@ library(psych)
 library(ez)
 curr_dir = getwd()
 
+core_dir = 'D:/Kristijan/raziskovalno/novel_meaning'
 inp_dir = 'D:/Kristijan/raziskovalno/novel_meaning/data/logs'
 out_dir = 'D:/Kristijan/raziskovalno/novel_meaning/output/beh'
 
@@ -40,8 +41,12 @@ for (i in 1:length(myfiles)){
   
 }
 
-sub_all <- head(sub_all[, -c(6, 10)])
-names(sub_all)[3] <- 'mood' 
+
+#####-------------------------SHAPING-THE-DATA
+
+sub_all <- sub_all[, -c(6, 10)]
+names(sub_all)[3] <- 'mood'
+names(sub_all)[4] <- 'paragrafnr'
 
 #####-------------------------SUMMARIES
 
@@ -63,16 +68,19 @@ summary_data <- ddply(sub_all, ~sID + mood + condition,
                       mode = my_mode(sens_rat))
 
 #factor the relevant columns
-summary_data$group <- factor(summary_data$mood)
-summary_data$condition <- factor(summary_data$condition)
+summary_data$mood <- factor(summary_data$mood, levels = c(1, 2), labels = c('happy', 'sad'))
+summary_data$condition <- factor(summary_data$condition, levels = c(1, 2), labels = c('sens', 'nsens'))
 
-summary_means <- describeBy(summary_data$mean, list(summary_data$group, summary_data$condition))
+summary_split <- split(summary_data, f = list(summary_data$mood, summary_data$condition))
 
+#table of means by group and condition
+summary_means <- describeBy(summary_data$mean, list(summary_data$mood, summary_data$condition))
+summary_agg <- aggregate(summary_data$mean, by = list(summary_data$mood, summary_data$condition), FUN = mean)
 #####-------------------------PLOTS
 
-sum_plot <- ggplot(data = summary_data,
-                          aes(x = condition, y = mean, color = group, group = group)) +
-                          geom_point(position = position_jitter(width = 0.08)) +
+sum_plot <- ggplot(data = summary_agg,
+                          aes(x = Group.2, y = x, color = Group.1, group = Group.1)) +
+                          geom_point() +
                           geom_line() +
                           labs(x = 'semantic condition', y = 'rating', color = 'mood') +
                           ggtitle('sensibility ratings') +
@@ -81,5 +89,14 @@ sum_plot <- ggplot(data = summary_data,
 sum_plot
 
 #####-------------------------SAVING
+
+#saving R objects
+save_data <- file.path(core_dir, 'preproc', 'sensibility_all.Rdata', fsep = '/')
+save_summary <- file.path(out_dir, 'stats/descriptive', 'summary_sensibility.Rdata', fsep = '/')
+
+save(sub_all, file = save_data) #save sensibility ratings for all subjects in one data frame
+save(summary_data, file = save_summary) #saving summary dataframe
+
+#saving plots & tables
 ggsave("mean_ratings.pdf", sum_plot, path = out_dir)
 write.table(summary_data, "ratings_sum.txt", quote = FALSE)
