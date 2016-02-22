1c46273d9fce6d975a0e249ccd2b15aa9db30bf2
KristijanArmeni <kristijan.armeni@gmail.com>
2016-02-22 22:34:35 +0100

    added t-test

--- sensibility_stat.R
+++ sensibility_stat.R
@@ -1,6 +1,6 @@
 #NOVEL_MEANING
 
-#this is the script for doing statistics on log and sensibility rating data
+#this is the script for doing statistics on sensibility rating data
 #by Kristijan Armeni
 
 #####-------------------------INITIALISATION
@@ -12,7 +12,7 @@ library(psych)
 library(ez)
 curr_dir = getwd()
 
-inp_dir = 'D:/Kristijan/raziskovalno/novel_meaning/data/beh/stats/descriptive'
+inp_dir = 'D:/Kristijan/raziskovalno/novel_meaning/output/beh/stats/descriptive'
 out_dir = 'D:/Kristijan/raziskovalno/novel_meaning/output/beh/stats/tests'
 
 # set proper dir for reading in the files
@@ -22,4 +22,13 @@ if (curr_dir != inp_dir){
 
 #loading the dataset
 load('sum_sensibility_persubjects.Rdata')
-data <- sel_data
+dat <- sum_dat_persub
+dat <- split(dat, f = list(dat$mood, dat$condition))
+dat2 <- split(sum_dat_persub, f = sum_dat_persub$mood)
+
+#####-------------------------STATS
+
+tS <- t.test(dat$happy.sens$mean, dat$happy.nsens$mean, paired = TRUE) 
+tH <- t.test(dat$sad.sens$mean, dat$sad.nsens$mean, paired = TRUE) 
+
+t3 <- t.test(dat2$happy$mean, dat2$sad$mean)
