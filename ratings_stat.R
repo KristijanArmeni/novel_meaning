2b4837ebcc71f4fb93b06005a96f134c22fb04da
KristijanArmeni <kristijan.armeni@gmail.com>
2016-02-23 18:11:46 +0100

    added t-tests and some fancy ANOVA stuff

--- ratings_stat.R
+++ ratings_stat.R
@@ -13,7 +13,7 @@ library(ez)
 curr_dir = getwd()
 
 inp_dir = 'D:/Kristijan/raziskovalno/novel_meaning/data/logs/Rdata_logs'
-out_dir = 'D:/Kristijan/raziskovalno/novel_meaning/output/beh'
+out_dir = 'D:/Kristijan/raziskovalno/novel_meaning/output/beh/stats/tests'
 
 # set proper dir for reading in the files
 if (curr_dir != inp_dir){
@@ -40,25 +40,42 @@ data_fullsplit <- split(ratings, f = list(ratings$scale, ratings$group))
 
 #####-------------------------STATS
 
-#test for normality
-
 #ANOVA
 des <- ezDesign(data = ratings, .(group), .(rating), col = .(scale))
 summary(des)
 
-an <- ezANOVA(ratings, .(rating), within = .(scale), between = .(group), wid = .(ID))
+#ezPlot
+pl <- ezPlot(data = ratings, x = .(scale), dv = .(rating), within = .(scale), between = .(group), wid = .(ID),
+             do_lines = F)
+pl
+
+an <- ezANOVA(data = ratings, dv = .(rating), within = .(scale), between = .(group), wid = .(ID), type = 3)
 summary(an)
 
-aov <- aov(data = ratings, formula = rating ~ group*scale + Error(ID/scale))
-summary(aov)
-
-#post-hoc t-test
+#post-hoc t-test: scale
 t0 <- t.test(data_fullsplit$BL.happy$rating, data_fullsplit$BL.sad$rating)      #baseline
 t1 <- t.test(data_fullsplit$mip1.happy$rating, data_fullsplit$mip1.sad$rating)  #mip1
 t2 <- t.test(data_fullsplit$mip3.happy$rating, data_fullsplit$mip3.sad$rating)  #mip2
 
+#post-hoc t-test group
+t3 <- t.test(data_fullsplit$BL.happy$rating, data_fullsplit$mip1.happy$rating) #happy; BL vs. mip1
+t4 <- t.test(data_fullsplit$mip1.happy$rating, data_fullsplit$mip3.happy$rating) #happy; mip1 vs. mip3
+t5 <- t.test(data_fullsplit$BL.sad$rating, data_fullsplit$mip1.sad$rating) #happy; BL vs. mip1
+t6 <- t.test(data_fullsplit$mip1.sad$rating, data_fullsplit$mip3.sad$rating) #happy; mip1 vs. mip3
+
 pvalues <- c(t0$p.value, t1$p.value, t2$p.value) #store just the p-values
 
 #adjust for multiple comparisons
-p_adjusted <- p.adjust(pvalues, method = "bonferroni")
+p_adjusted <- p.adjust(pvalues, method = "BH")
+
+#saving
+
+aovFile <- file.path(out_dir, "mood_rating_aov.txt") #create long filename for ANOVA results
+capture.output(an, file = aovFile) #save ANOVA results
+
+tFile <- file.path(out_dir, "mood_rating_t.txt")
+capture.output(t0, file = tFile)
+capture.output(t1, file = tFile, append = T)
+capture.output(t2, file = tFile, append = T)
+
 
